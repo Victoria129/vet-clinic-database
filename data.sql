39ed675a20fd51a95af9e9fd9f2b94ed6af88bad
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2023-04-06 22:54:32 +0300

    Update animals table

--- data.sql
+++ data.sql
@@ -6,3 +6,20 @@ VALUES
   (2, 'Gabumon', '2018-11-15', 2, true, 8.0),
   (3, 'Pikachu', '2021-01-07', 1, false, 15.04),
   (4, 'Devimon', '2017-05-12', 5, true, 11.0);
+
+/* Day two queries */
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (5, 'Charmander', '2020-02-08', 0, 'f', -11.0);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (6, 'Plantmon', '2021-11-15', 2, 't', -5.7);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (7, 'Squirtle', '1993-04-02', 3, 'f', -12.13);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (8, 'Angemon', '2005-06-12', 1, 't', -45.0);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (9, 'Boarmon', '2005-06-07', 7, 't', 20.4);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (10, 'Blossom', '1998-10-13', 3, 't', 17.0);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (11, 'Ditto', '2022-05-14', 4, 't', 22.0);
--- schema.sql
+++ schema.sql
@@ -8,3 +8,8 @@ CREATE TABLE animals (
    neutered BOOLEAN DEFAULT FALSE,
    weight_kg DECIMAL(5,2) NOT NULL
 );
+
+/* Day two queries */
+
+ALTER TABLE animals ADD COLUMN species VARCHAR(50);
+
